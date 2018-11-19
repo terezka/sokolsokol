0a8856a454085713e9bd70154092c612272f1ba5
Tereza Sokol <terezasokol@gmail.com>
2018-11-19 17:56:22 -0500

    Add essay style

--- src/Page/Article.elm
+++ src/Page/Article.elm
@@ -30,8 +30,8 @@ view model =
     { title = "SOKOL SOKOL | Kafka, Civilization, and Bureaucracy"
     , body =
         [ Html.article
-            [ Attr.css [ Css.maxWidth (Css.px 700) ] ]
-            [ Html.h1 [ Attr.css [ Css.textDecoration Css.overline ] ] [ Html.text "Kafka, Civilization, and Bureaucracy" ]
+            [ Attr.css [ Css.maxWidth (Css.px 780), Css.property "column-count" "2" ] ]
+            [ Html.img [ Attr.src "https://spectrum.ieee.org/image/Mjc5MDM4Nw.jpeg", Attr.css [ Css.width (Css.pct 100) ] ] [] , Html.h1 [ Attr.css [ Css.textDecoration Css.overline ] ] [ Html.text "Kafka, Civilization, and Bureaucracy" ]
             , Html.p [] [ Html.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et fermentum massa. Proin rutrum suscipit finibus. Sed consequat, est at blandit accumsan, neque turpis gravida nulla, ac cursus arcu lorem a eros. Integer purus libero, imperdiet ac ligula quis, porttitor mattis leo. Vivamus laoreet elit at ante iaculis fringilla. Mauris nec imperdiet magna. Maecenas finibus urna in ex sodales, vitae porta turpis scelerisque." ]
             , Html.p [] [ Html.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et fermentum massa. Proin rutrum suscipit finibus. Sed consequat, est at blandit accumsan, neque turpis gravida nulla, ac cursus arcu lorem a eros. Integer purus libero, imperdiet ac ligula quis, porttitor mattis leo. Vivamus laoreet elit at ante iaculis fringilla. Mauris nec imperdiet magna. Maecenas finibus urna in ex sodales, vitae porta turpis scelerisque." ]
             , Html.p [] [ Html.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et fermentum massa. Proin rutrum suscipit finibus. Sed consequat, est at blandit accumsan, neque turpis gravida nulla, ac cursus arcu lorem a eros. Integer purus libero, imperdiet ac ligula quis, porttitor mattis leo. Vivamus laoreet elit at ante iaculis fringilla. Mauris nec imperdiet magna. Maecenas finibus urna in ex sodales, vitae porta turpis scelerisque." ]
@@ -42,6 +42,7 @@ view model =
             , Html.p [] [ Html.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et fermentum massa. Proin rutrum suscipit finibus. Sed consequat, est at blandit accumsan, neque turpis gravida nulla, ac cursus arcu lorem a eros. Integer purus libero, imperdiet ac ligula quis, porttitor mattis leo. Vivamus laoreet elit at ante iaculis fringilla. Mauris nec imperdiet magna. Maecenas finibus urna in ex sodales, vitae porta turpis scelerisque." ]
             , Html.p [] [ Html.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et fermentum massa. Proin rutrum suscipit finibus. Sed consequat, est at blandit accumsan, neque turpis gravida nulla, ac cursus arcu lorem a eros. Integer purus libero, imperdiet ac ligula quis, porttitor mattis leo. Vivamus laoreet elit at ante iaculis fringilla. Mauris nec imperdiet magna. Maecenas finibus urna in ex sodales, vitae porta turpis scelerisque." ]
             , Html.p [] [ Html.text "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et fermentum massa. Proin rutrum suscipit finibus. Sed consequat, est at blandit accumsan, neque turpis gravida nulla, ac cursus arcu lorem a eros. Integer purus libero, imperdiet ac ligula quis, porttitor mattis leo. Vivamus laoreet elit at ante iaculis fringilla. Mauris nec imperdiet magna. Maecenas finibus urna in ex sodales, vitae porta turpis scelerisque." ]
+            , Html.i [] [ Html.text "By Tereza Sokol" ]
             ]
         ]
     }
