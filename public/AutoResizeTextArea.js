550ce3bc4872d8c89c0d11853b9fd8d6153d9117
Tereza Sokol <terezasokol@gmail.com>
2018-11-23 18:03:56 -0500

    Simplify auto resize

--- public/AutoResizeTextArea.js
+++ public/AutoResizeTextArea.js
@@ -9,6 +9,17 @@ class AutoResizeTextArea extends HTMLElement {
     this._autoresize = false;
     this._updateListener.bind(this);
     this._resize.bind(this);
+
+    this._textarea.style.resize = "none";
+    this._textarea.style.outline = "none";
+    this._textarea.style.border = "none";
+    this._textarea.style.textDecoration = "inherit";
+    this._textarea.style.fontWeight = "inherit";
+    this._textarea.style.fontSize = "inherit";
+    this._textarea.style.width = "100%";
+    this._textarea.style.overflowY = "hidden";
+    this._textarea.style.transition = "none";
+    this._textarea.style.padding = "0";
   }
 
   attributeChangedCallback(name, previous, next) {
@@ -17,19 +28,19 @@ class AutoResizeTextArea extends HTMLElement {
       if (!this._textarea) return;
       this._updateListener();
     } else if (name == "data-value") {
-      if (!this._textarea) return;
       this._textarea.value = this.getAttribute("data-value");
-      this._updateListener();
     } else if (name == "data-placeholder") {
-      if (!this._textarea) return;
       this._textarea.placeholder = this.getAttribute("data-placeholder");
-      this._updateListener();
     }
   }
 
   connectedCallback() {
     this._updateListener();
     this.appendChild(this._textarea);
+
+    this._textarea.rows = 1;
+    this._textarea.baseHeight = this._textarea.offsetHeight;
+    this._textarea.rows = undefined;
   }
 
   _updateListener() {
@@ -43,31 +54,9 @@ class AutoResizeTextArea extends HTMLElement {
 
   _resize() {
     setTimeout(() => {
-      var minHeight = null;
-      if (this._textarea.style.minHeight) {
-        minHeight = parseInt(this._textarea.style.minHeight, 10);
-      } else {
-        minHeight = parseInt(window.getComputedStyle(this._textarea).minHeight, 10);
-      }
-      if (minHeight === 0) {
-        minHeight = parseInt(window.getComputedStyle(this._textarea).height, 10);
-      }
-
-      this._textarea.style.resize = "none";
-      this._textarea.style.outline = "none";
-      this._textarea.style.border = "none";
-      this._textarea.style.textDecoration = "inherit";
-      this._textarea.style.fontSize = "inherit";
-      this._textarea.style.fontWeight = "inherit";
-      this._textarea.style.width = "100%";
-      this._textarea.style.overflowY = "hidden";
-      this._textarea.style.minHeight = minHeight + "px";
-      this._textarea.style.transition = "none";
-      if (this._textarea.scrollHeight > minHeight) {
-        this._textarea.style.height = minHeight + "px";
-        this._textarea.style.height = this._textarea.scrollHeight + "px";
-      } else {
-        this._textarea.style.height = minHeight + "px";
+      var rows = Math.ceil(this._textarea.scrollHeight / this._textarea.baseHeight);
+      if (this._textarea.rows !== rows) {
+        this._textarea.rows = rows;
       }
     }, 0);
   }
